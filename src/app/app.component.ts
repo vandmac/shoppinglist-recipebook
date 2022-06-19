ef8a4bbded2537471df7fbea1e8d50cb0e0eb2f4
LGGRAMAM\Andrzej <vandmac@gmail.com>
2022-06-19 12:11:24 +0200

    finish debugging training

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+//here is the error - servers array is not defined
+// servers;
+  servers=[];
+
+  onAddServer() {
+    this.servers.push('Another Server');
+  }
+
+  onRemoveServer(id: number) {
+    // const position = id + 1;
+    const position = id;
+    this.servers.splice(position, 1);
+  }
 }
